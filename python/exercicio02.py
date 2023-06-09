3108667c389ef67a3aa4d4ecda832a837a081254
Kaic <kaicoliveirarodrigues2@gmail.com>
2023-06-09 17:24:37 -0300

    Create exercicio02.py

---
+++ python/exercicio02.py
@@ -0,0 +1,7 @@
+# Faça um programa que receba a idade do usuário e diga se já pode tira a habilitação
+
+idade = int(input("Digite sua idade: "))
+if(idade >= 18):
+    print("Já pode tira a habilitação")
+else:
+    print("Não pode tira a habilitação")
