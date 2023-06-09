e039becd7ccfc9f775b11eae218251d4a741009e
Kaic <kaicoliveirarodrigues2@gmail.com>
2023-06-09 17:20:03 -0300

    Update programa07.py

--- python/programa07.py
+++ python/programa07.py
@@ -4,5 +4,8 @@ if(True):
   print("Código passou pelo if")
 
   idade = int(input("Digite sua idade: "))
+
   if(idade >= 18):
     print("Você é maior de idade")
+  else:
+    print("Você é menor de idade")
