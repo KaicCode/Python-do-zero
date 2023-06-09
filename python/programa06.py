f4cf9a29152d36bad11091b687136cfdb8be183d
Kaic <kaicoliveirarodrigues2@gmail.com>
2023-06-09 17:07:40 -0300

    Create programa06.py

---
+++ python/programa06.py
@@ -0,0 +1,18 @@
+# Operadores de comparações
+
+# iguais == 
+# Diferente !=
+# Maior >
+# Menor <
+# Maior ou igual >=
+# Menor ou igual <=
+
+a = 10 
+b = 5
+
+print(a == b)
+print(a != b)
+print(a > b)
+print(a < b)
+print(a >= b)
+print(a <= b)
